5bd48f39bcfb6dc9e77427d7826e6513ed10c5e8
phillip-tech <pm.bogopane@gmail.com>
2024-09-26 16:01:58 +0200

    Added image domains to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['next-ecommerce-api.vercel.app', 'cdn.dummyjson.com'],
+    },
+  };
+  
+  export default nextConfig;
