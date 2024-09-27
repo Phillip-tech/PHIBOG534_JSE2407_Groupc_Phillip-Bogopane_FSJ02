57bc2dbe28af14c135199cc9fe35da650974a6fb
phillip-tech <pm.bogopane@gmail.com>
2024-09-27 10:43:33 +0200

    Added image gallery component with main image and thumbnails

--- app/components/ProductGallery.jsx
+++ app/components/ProductGallery.jsx
@@ -13,3 +13,37 @@ const ProductGallery = ({ images }) => {
       </div>
     );
   }
+  
+  return (
+    <div className="space-y-4">
+      <div
+        className="aspect-w-16 aspect-h-9 relative rounded-lg overflow-hidden cursor-pointer"
+        onClick={() => setIsModalOpen(true)}
+      >
+        <Image
+          src={images[currentIndex]}
+          alt="Product"
+          width={800} 
+          height={600}
+          objectFit="contain"
+          className="transition-opacity duration-300"
+        />
+      </div>
+      <div className="flex space-x-2 overflow-x-auto">
+        {images.map((image, index) => (
+          <button
+            key={index}
+            onClick={() => setCurrentIndex(index)}
+            className={`flex-shrink-0 w-20 h-20 relative rounded-md overflow-hidden ${
+              index === currentIndex ? 'ring-2 ring-blue-500' : ''
+            }`}
+          >
+            <Image
+              src={image}
+              alt={`Thumbnail ${index + 1}`}
+              layout="fill"
+              objectFit="contain"
+            />
+          </button>
+        ))}
+      </div>
