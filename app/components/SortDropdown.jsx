68b2eb23bf2a45fda773d405deadca7d383020e6
phillip-tech <pm.bogopane@gmail.com>
2024-09-29 17:48:27 +0200

    Created JSDoc comments to SortDropdown

--- app/components/SortDropdown.jsx
+++ app/components/SortDropdown.jsx
@@ -2,6 +2,14 @@
 
 import { useState, useEffect } from 'react';
 
+/**
+ * The SortDropdown component displays a dropdown menu for selecting a sorting option.
+ *
+ * @param {Object} props - The component props.
+ * @param {Function} props.onSortChange - A callback function that is called when the user selects a sorting option.
+ * @param {string} [props.initialValue] - The initial selected sorting option.
+ * @returns {JSX.Element} - The SortDropdown component.
+ */
 const SortDropdown = ({ onSortChange, initialValue }) => {
   const [sortOption, setSortOption] = useState(initialValue || '');
 
